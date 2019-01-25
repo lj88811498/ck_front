5d2f6a0576f132af5fa917ff47ffb334fc479d56
冯晔晨 <fengyechen@youedata.com>
2019-01-25 11:34:12 +0800

    域名

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"http://172.16.0.181:8888"',
-  URL_HOST: '"http://47.104.162.57:8888"'
+  API_HOST: '"http://47.104.162.57:8888"',
+  //URL_HOST: '"/"'
+  URL_HOST: '"http://www.hxra.cn"'
 }
